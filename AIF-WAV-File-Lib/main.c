36b86cbb8d0c34f4244fcd3c5e29e114411ff6a0
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2014-11-29 12:19:43 +0000

    Initial Commit

---
+++ AIF-WAV-File-Lib/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  AIF-WAV-File-Lib
+//
+//  Created by Pierre Alexandre Tremblay on 29/11/14.
+//  Copyright (c) 2014 Pierre Alexandre Tremblay. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
