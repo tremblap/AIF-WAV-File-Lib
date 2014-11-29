d4b59d50bee105050c023c4ad4e3c797ea262926
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2014-11-29 12:55:13 +0000

    getting ready for header munching

--- AIF-WAV-File-Lib/main.c
+++ AIF-WAV-File-Lib/main.c
@@ -7,12 +7,28 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, const char * argv[])
 {
-    int n;
+    int n, SR, nbchan, samptype;
+    
     for (n=0;n<argc;n++)
-        printf("argc %d is %s\r", n, argv[n]);
+    {
+        //printf("argc %d is %s\r", n, argv[n]);
+        //printf ("%d\r", strcmp (argv[n],"-header"));
+        
+        if (strcmp (argv[n],"-header") == 0)
+        {
+            printf("in\r");
+            SR = argv[n + 1];
+            nbchan = argv[n + 2];
+            samptype = argv[n +3];
+        }
+    }
+    printf("SR = %d\rnbchan = %d\rsamptype = %d\r", SR, nbchan, samptype);
+    
+    
     return 0;
 }
 
