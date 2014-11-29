30cc3eac6a6037b83563d9724fab982283f87f69
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2014-11-29 16:40:32 +0100

    forking the int vs float as is - needs to change the header import to accommodate 2 lengths

--- AIF-WAV-File-Lib/main.c
+++ AIF-WAV-File-Lib/main.c
@@ -11,7 +11,7 @@
 
 int main(int argc, const char * argv[])
 {
-    unsigned int n, nbchan, samptype;
+    unsigned int n, nbchan, sampdepth;
     FILE *inputfile = NULL;
     unsigned char WavHead[12], WavFmt[32];
     unsigned long datachunksize = 0L, SR;
@@ -26,7 +26,7 @@ int main(int argc, const char * argv[])
             printf("in\r");
             SR = argv[n + 1];
             nbchan = argv[n + 2];
-            samptype = argv[n +3];
+            sampdepth = argv[n +3];
         }
         else
         {
@@ -70,31 +70,47 @@ int main(int argc, const char * argv[])
         return -3;
     };
     
-    //check if PCM
-    if (WavFmt[8]!= 1 | WavFmt[9]!= 0)
+    //check if PCM or float
+    if (WavFmt[9]== 0)
+    {
+        
+        if (WavFmt[8] == 1)
+        {
+            //safety check the datachunk
+            if (strncmp(WavFmt+24, "data", 4))
+            {
+                printf("Nodata\r");
+                fclose(inputfile);
+                return -3;
+            }
+            //transfer useful data from the chunk
+            
+            nbchan = (unsigned int)WavFmt[11]<<8 | (unsigned int)WavFmt[10];
+            SR =(unsigned long)WavFmt[15]<<24 | (unsigned long)WavFmt[14]<<16 | (unsigned long)WavFmt[13]<<8 | (unsigned long)WavFmt[12];
+            sampdepth =(unsigned int)WavFmt[23]<<8 | (unsigned int)WavFmt[22];
+            datachunksize = (unsigned long)WavFmt[31]<<24 | (unsigned long)WavFmt[30]<<16 | (unsigned long)WavFmt[29]<<8 | (unsigned long)WavFmt[28];
+
+        }
+        else if (WavFmt[8] == 3)
+        {
+            printf("thisisafloat\r");
+            printf("extchunksize = %d",((unsigned int)WavFmt[25]<<8 | (unsigned int)WavFmt[24]));
+        }
+        else
+        {
+            printf("NoINTofFLOAT\r");
+            fclose(inputfile);
+            return -3;
+        }
+    }
+    else
     {
         printf("NoPCM\r");
         fclose(inputfile);
         return -3;
     };
-
-    //safety check the datachunk
-    if (strncmp(WavFmt+24, "data", 4))
-    {
-        printf("Nodata\r");
-        fclose(inputfile);
-        return -3;
-    };
-
-    //transfer useful data from the chunk
     
-    nbchan = (unsigned int)WavFmt[11]<<8 | (unsigned int)WavFmt[10];
-    SR =(unsigned long)WavFmt[15]<<24 | (unsigned long)WavFmt[14]<<16 | (unsigned long)WavFmt[13]<<8 | (unsigned long)WavFmt[12];
-    samptype =(unsigned int)WavFmt[23]<<8 | (unsigned int)WavFmt[22];
-    datachunksize = (unsigned long)WavFmt[31]<<24 | (unsigned long)WavFmt[30]<<16 | (unsigned long)WavFmt[29]<<8 | (unsigned long)WavFmt[28];
-    
-    
-    printf("SR = %ld samps/sec\rnbchan = %d\rsamptype = %d bit per sample\r", SR, nbchan, samptype);
+    printf("SR = %ld samps/sec\rnbchan = %d\rsampdepth = %d bit per sample\r", SR, nbchan, sampdepth);
     printf("size of data chunk = %ld bytes of audio\r",datachunksize);
 
     
