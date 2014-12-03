e9b246a989ac70b6a041e13b60c9cb984c8a56e3
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2014-12-03 18:24:45 +0100

    removed the GLPL version of extended.h, and created a new extended.h which is simply defining Apple's free conversion routines

--- AIF-WAV-File-Lib/extended.h
+++ AIF-WAV-File-Lib/extended.h
@@ -1,34 +1,7 @@
-/*
- Audio File Library
- Copyright (C) 1998, Michael Pruett <michael@68k.org>
- 
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Library General Public
- License as published by the Free Software Foundation; either
- version 2 of the License, or (at your option) any later version.
- 
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Library General Public License for more details.
- 
- You should have received a copy of the GNU Library General Public
- License along with this library; if not, write to the
- Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- Boston, MA  02111-1307  USA.
- */
+// defines Apple's routines for the compiler
 
-/*
- extended.h
- 
- This file defines interfaces to Apple's extended floating-point
- conversion routines.
- */
-
-#ifndef EXTENDED_H
-#define EXTENDED_H
-
+// not needed but in the legally uneditable extended.c
 void _af_convert_to_ieee_extended (double num, unsigned char *bytes);
-double _af_convert_from_ieee_extended (const unsigned char *bytes);
 
-#endif
+// this is what we need
+double _af_convert_from_ieee_extended (const unsigned char *bytes);
